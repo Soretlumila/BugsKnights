ecf6bb6fa2e853af5d5dbc3cdc7f41c2ff0b83b4
SoretLumila <larmetlouis@gmail.com>
2026-01-15 16:55:35 +0100

    finir header et footer et home

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -54,7 +54,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', User::class);
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', Cards::class);
+        yield MenuItem::linkToCrud('Users Admin', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Add Product', 'fas fa-list', Cards::class);
     }
 }
